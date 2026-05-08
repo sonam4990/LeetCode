5159f5171cc0557b1f367e7b16ab866454f8032d
Sonam Kumari <105632873+sonam4990@users.noreply.github.com>
2026-05-08 23:07:07 +0530

    Time: 642 ms (97.17%), Space: 146.3 MB (17.29%) - LeetHub

--- 3629-minimum-jumps-to-reach-end-via-prime-teleportation/3629-minimum-jumps-to-reach-end-via-prime-teleportation.py
+++ 3629-minimum-jumps-to-reach-end-via-prime-teleportation/3629-minimum-jumps-to-reach-end-via-prime-teleportation.py
@@ -1,95 +1,40 @@
-from typing import List
-from collections import defaultdict, deque
+MX = 1_000_001
+factors = [[] for _ in range(MX)]
+for i in range(2, MX):
+    if not factors[i]:
+        for j in range(i, MX, i):
+            factors[j].append(i)
+
 
 class Solution:
     def minJumps(self, nums: List[int]) -> int:
-
         n = len(nums)
+        edges = defaultdict(list)
+        for i, a in enumerate(nums):
+            if len(factors[a]) == 1:
+                edges[a].append(i)
+        res = 0
+        seen = [False] * n
+        seen[-1] = True
+        q = [n - 1]
+        while True:
+            q2 = []
+            for i in q:
+                if i == 0:
+                    return res
+                if i > 0 and not seen[i - 1]:
+                    seen[i - 1] = True
+                    q2.append(i - 1)
+                if i < n - 1 and not seen[i + 1]:
+                    seen[i + 1] = True
+                    q2.append(i + 1)
+                for p in factors[nums[i]]:
+                    for j in edges[p]:
+                        if not seen[j]:
+                            seen[j] = True
+                            q2.append(j)
+                    edges[p].clear()
+            q = q2
+            res += 1
 
-        # ---------- Prime Check ----------
-        def is_prime(x):
 
-            if x < 2:
-                return False
-
-            i = 2
-
-            while i * i <= x:
-
-                if x % i == 0:
-                    return False
-
-                i += 1
-
-            return True
-
-        # ---------- Build Factor Map ----------
-        factor_map = defaultdict(list)
-
-        for i, num in enumerate(nums):
-
-            x = num
-            d = 2
-
-            while d * d <= x:
-
-                if x % d == 0:
-
-                    factor_map[d].append(i)
-
-                    while x % d == 0:
-                        x //= d
-
-                d += 1
-
-            if x > 1:
-                factor_map[x].append(i)
-
-        # ---------- BFS ----------
-        q = deque([0])
-
-        visited = [False] * n
-        visited[0] = True
-
-        used_prime = set()
-
-        steps = 0
-
-        while q:
-
-            for _ in range(len(q)):
-
-                idx = q.popleft()
-
-                if idx == n - 1:
-                    return steps
-
-                # Move Left
-                if idx - 1 >= 0 and not visited[idx - 1]:
-
-                    visited[idx - 1] = True
-                    q.append(idx - 1)
-
-                # Move Right
-                if idx + 1 < n and not visited[idx + 1]:
-
-                    visited[idx + 1] = True
-                    q.append(idx + 1)
-
-                # Prime Teleport
-                val = nums[idx]
-
-                if is_prime(val) and val not in used_prime:
-
-                    for nxt in factor_map[val]:
-
-                        if not visited[nxt]:
-
-                            visited[nxt] = True
-                            q.append(nxt)
-
-                    used_prime.add(val)
-
-            steps += 1
-
-        return -1
