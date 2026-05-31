5b7e67c8685324999173eef40f719c0f963fdd23
Sonam Kumari <105632873+sonam4990@users.noreply.github.com>
2026-05-31 12:35:50 +0530

    Time: 76 ms (45.87%), Space: 33.8 MB (75.43%) - LeetHub

---
+++ 2126-destroying-asteroids/2126-destroying-asteroids.py
@@ -0,0 +1,13 @@
+class Solution:
+    def asteroidsDestroyed(self, mass: int, asteroids: list[int]) -> bool:
+
+        asteroids.sort()
+
+        for asteroid in asteroids:
+
+            if mass < asteroid:
+                return False
+
+            mass += asteroid
+
+        return True
